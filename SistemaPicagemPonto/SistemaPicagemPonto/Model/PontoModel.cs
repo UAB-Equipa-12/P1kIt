1cc05e44bfca5085240c2f978e4a696059366943
Rui Manuel Araújo <rmaraujo@f3m.pt>
2026-04-15 12:19:33 +0100

    implementação RegistarSaida e GetPassword

--- SistemaPicagemPonto/SistemaPicagemPonto/Model/PontoModel.cs
+++ SistemaPicagemPonto/SistemaPicagemPonto/Model/PontoModel.cs
@@ -7,6 +7,11 @@
 
         public delegate void RegistosAtualizados(object origem);
         public event RegistosAtualizados AlteracaoRegistos;
+        private readonly List<Colaborador> Colaboradores =
+        [
+            new Colaborador { Id = 1, Nome = "Colaborador1", Password = "1234" },
+            new Colaborador { Id = 42, Nome = "Colaborador2", Password = "abcd" }
+        ];
 
         public PontoModel()
         {
@@ -32,12 +37,30 @@
 
         public void RegistarSaida(int colaboradorId)
         {
+            RegistoPonto? registoAberto = registos
+                .LastOrDefault(r => r.IdColaborador == colaboradorId && r.HoraSaida == null);
+
+            if (registoAberto == null)
+                return;
+
+            registoAberto.HoraSaida = DateTime.Now;
+
+            repo.Guardar(registos);
+
             AlteracaoRegistos?.Invoke(this);
         }
 
         public void ObterRegistos(ref List<RegistoPonto> lista)
         {
             lista = [.. registos];
+        }
+
+        public string? GetPassword(string username)
+        {
+            Colaborador? c = Colaboradores
+                .FirstOrDefault(c => c.Nome.Equals(username, StringComparison.OrdinalIgnoreCase));
+
+            return c?.Password;
         }
     }
 }
